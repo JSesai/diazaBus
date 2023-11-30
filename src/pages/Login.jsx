92dbcdeb29bc119bb199146a23fb147d5df004cb
Omar Asael Hernandez Montiel <51717542+omarasael1980@users.noreply.github.com>
2023-11-30 10:39:04 -0800

    Conexion a Backend, Tailwind, ContextAPI, loguin, recuperacion de password, Creacion de Usuarios, rutas publicas, Rutas protegidas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,148 +1,126 @@
 import { useState, useEffect } from "react";
-import { Box, Card, Typography, TextField, Button, Dialog } from "@mui/material"
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import axiosClient from "../config/axiosClient";
 import LOGO_DIAZA from "../assets/LOGO_DIAZA.png"
-
+import Alert from "../components/Alert";
+import useAuth from "../hooks/useAuth";
 export default function Login() {
 
 
-    //estates
-    const [user, setUser] = useState({ name: "", password: "" });
-    const [userName, setUserName] = useState("");
-    const [userPassword, setUserPassword] = useState("");
-    const [buttonNext, setButtonNext] = useState(true);
-    const [userNameField, setUserNameField] = useState(false);
-    const [userPasswordField, setUserPasswordField] = useState(false);
-    
+    //states
+     
+    const [password, setPassword] = useState("")
+    const [email, setEmail]= useState("")
+    const [alertmsg, setAlertMsg] = useState("")
+    //destructuring ContextAPI
+    const {setAuth}=useAuth()
+ 
 const navigate = useNavigate();
-    const handleSubmit = () => {
-        try {
-            //hacer peticion y enviar use y passwor para validacion
-
-            //si el loguin es correcto navegar a mainhub
-            navigate("/mainhub");
-        } catch (error) {
-            //si el logiuin es incorrecto mostrar alerta 
-            
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        //check if any fields are empty
+        if([email, password].includes('')){
+            setAlertMsg({
+                msg: "Todos los campos son obligatorios",
+                error: true
+            })
+            return
         }
 
-
-
-
-
-
+        try {
+            //send info to backend to check password and user validation
+            const {data} = await axiosClient.post('/usuarios/login',{
+                email, password
+            } )
+            //save token in local storage
+            localStorage.setItem('token',data.token)
+            //save data from login in context api
+            setAuth(data)
+                      //clean Alert and fields 
+            setAlertMsg({})
+            setEmail("")
+            setPassword("")
+            //if email and password are valid
+           navigate("/mainhub");
+        } catch (error) {
+            //si el logiuin es incorrecto mostrar alerta
+            setAlertMsg({
+                msg: "Error de login: " + error.response.data.msg,
+                error: true
+            })
+            
+        }
     }
 
-
-    // validar input nombre y setear 
-    const handleUserNameChange = (event) => {
-        event.target.value === '' ? setUserNameField(false) : setUserNameField(true);        
-        (userNameField && userPasswordField) && setButtonNext(false) ;
-        setUserName(event.target.value);
-    };
-
-    // Al entrar a la función, seteamos el valor de user con los datos del campo Password
-    const handleUserPasswordChange = (event) => {
-        event.target.value === '' ? setUserPasswordField(false) : setUserPasswordField(true);
-        (userNameField && userPasswordField) && setButtonNext(false);
-        console.log(userPasswordField);
-        setUserPassword(event.target.value);
-        
-    };
-
-
+  const {msg} =alertmsg
+   
     return (
         <>
 
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                height='100%'
-            >
-                <Card
-                    style={{
-                        borderRadius: "20px",
-                        width: "610px",
-                        height: '377px',
-                        padding: "24px 4px",
-                        boxShadow:
-                            "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px"
-                    }}
-                >
+            <div className=" w-1/2 mx-auto justify-center items-center   p-5 ">
+                <div
+                    className="rounded-md px-3 py-5 shadow-md ">
                     {/* Logo */}
                     <figure
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            margin: '0px'
-                        }}
-                    >
+                        className="flex justify-center items-center  shadow-sm">
                         <img
                             src={LOGO_DIAZA}
                             alt="Logo de Diaza"
-                            height={120}
-                            width={120}
+                            height={200}
+                            width={200}
                         />
                     </figure>
 
                     {/* Input */}
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            padding: "0 55px",
-                        }}
-                    >
-                        <div>
-                            <Typography
-                                color="#024995"
-                                style={{
-                                    padding: "13px 0 0 0",
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    fontFamily: 'roboto',
-                                    fontSize: '15px'
-                                }}
-                            >
-                                <span>Ingresa tus datos para iniciar sesión</span>
-                            </Typography>
-
-                            <TextField
-                                label="Usuario"
-                                fullWidth
-                                value={userName}
-                                onChange={handleUserNameChange}
-                                style={{ margin: "13px 0px 8px 0px" }}
-                            />
-
-                            <TextField
-                                id="outlined-read-only-input"
-                                label="Contraseña"
-                                type="password"
-                                fullWidth
-                                value={userPassword}
-                                onChange={handleUserPasswordChange}
-                                style={{ margin: "5px 0 21px 0" }}
-                            />
-
-                            <Button
-                                onClick={handleSubmit}
-                                variant="contained"
-                                size="large"
-                                style={{ margin: "15px 0 29.38px 0" }}
-                                disabled={buttonNext}
-                            >
-                                Iniciar sesión
-                            </Button>
-
-                        </div>
+                    <div className="flex flex-row justify-center   py-5">
+                        <p className="text-3xl font-bold text-sky-700">Inicia  
+                        <span className="text-cyan-950 text-3xl font-extrabold">      Sesión</span></p>
                     </div>
-                </Card>
-            </Box>
-
+                 <div>{msg && <Alert alert={alertmsg}/>}</div>   
+    <form 
+      onSubmit={handleSubmit}
+      className=" bg-white       p-5 w-full"
+      autoComplete="off">
+      <div className="  bg-white   " >
+        <label  className="text-xl uppercase text-gray-300 block font-bold" htmlFor="email">Email:  </label>
+        <input 
+          type="email"
+          placeholder="Ingresa tu correo"
+          name="email"
+          autoComplete="email"
+          id="email"
+          onChange={e=>setEmail(e.target.value)}
+          value={email}
+          className="text-xl  w-full mt-3 p-3 border rounded-xl bg-gray-50"
+        />
+      </div>
+      <div className="  bg-white   " >
+        <label  className="text-xl uppercase text-gray-300 block font-bold" htmlFor="email">Password:  </label>
+        <input 
+          type="password"
+          placeholder="Password  "
+          name="password"
+          id="password"
+          autoComplete="new-password"
+          onChange={e=>setPassword(e.target.value)}
+          value={password}
+          className="text-xl  w-full   p-3 border rounded-xl bg-gray-50"
+        />
+      </div>
+      <input type="submit" value="Iniciar Sesión" 
+       className="bg-sky-700 mt-5 p-2 text-white w-full rounded-md  
+       hover:cursor-pointer hover:bg-cyan-950 transition-colors uppercase font-bold" />
+                
+             </form>    
+             <Link 
+                className="flex justify-center items-center   text-cyan-600 "
+                       
+                     to="olvide-password"
+                    >Olvide mi password!</Link>  
+                </div>
+               
+            </div>
+             
         </>
     )
 }
