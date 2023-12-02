6deee05b416baee772c956da848278ab907c9e0f
Julio Sesai <julio.antonio@bamaglobal.com>
2023-12-02 11:50:51 -0600

    fix front

--- src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as App } from './App';
-export * from './index';
-export { default as AuthMainHub } from './layouts/AuthMainHub';
-export * from './main';
-export { default as Login } from './pages/Login';
-export { default as MainHub } from './pages/MainHub';
--- src/pages/mainHub.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function mainHub() {
-  return (
-    <div>
-      desde main hub
-    </div>
-  )
-}
